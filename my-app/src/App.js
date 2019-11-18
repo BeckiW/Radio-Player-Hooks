85da140124db2fccdf1aaec0cf5262613fc849f3
Becki Wordsworth <r.a.wordsworth@gmail.com>
2019-11-18 22:24:22 +0100

    Removing dead code and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Station from "../src/components/Channels/index";
+
 class App extends React.Component {
   render() {
     return (
       <div className="app">
-        <header>
+        <header className="headerBox">
           <h1>Sverige Radio Stations</h1>
         </header>
         <Station />
